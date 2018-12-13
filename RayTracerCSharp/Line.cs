323f44713a371e811d3fb8d569aa065fa6170e2f
Joe4422 <josephdotbrown@gmail.com>
2018-12-13 11:34:39 +0000

    Created classes to represent a Point, a Line and a LineEquation

---
+++ RayTracerCSharp/Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracerCSharp
+{
+    public class Line
+    {
+        public Point FirstPoint { get; private set; }
+        public Point SecondPoint { get; private set; }
+
+        public Line(Point firstPoint, Point secondPoint)
+        {
+            FirstPoint = firstPoint;
+            SecondPoint = secondPoint;
+        }
+
+        public LineEquation GetEquation()
+        {
+            double diffX = FirstPoint.X - SecondPoint.X;
+            double diffY = FirstPoint.Y - SecondPoint.Y;
+            double m = diffY / diffX;
+            double c = FirstPoint.Y - m * FirstPoint.X;
+            return new LineEquation(m, c);
+        }
+    }
+}
---
+++ RayTracerCSharp/LineEquation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracerCSharp
+{
+    public class LineEquation
+    {
+        public double Gradient { get; private set; }
+        public double Offset { get; private set; }
+
+        public LineEquation(double gradient, double offset)
+        {
+            Gradient = gradient;
+            Offset = offset;
+        }
+    }
+}
